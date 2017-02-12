c194d369e09f0a79e32d553e7a53aeee42ed7428
Cody Lambert <github@codylambert.com>
2017-02-11 23:52:49 -0500

    Add route for code of conduct

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
 root 'home#index'
 
 get 'home', to: 'home#index'
+get 'code-of-conduct', to: 'home#code-of-conduct'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
